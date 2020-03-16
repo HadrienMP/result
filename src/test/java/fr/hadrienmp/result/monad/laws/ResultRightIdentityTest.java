839f706cace4b08dceee2a7d83862fc9ca5ecaae
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-03-16 08:48:49 +0100

    Fix java 8 build

--- src/test/java/fr/hadrienmp/result/monad/laws/ResultRightIdentityTest.java
+++ src/test/java/fr/hadrienmp/result/monad/laws/ResultRightIdentityTest.java
@@ -15,12 +15,12 @@ public class ResultRightIdentityTest {
     @Test
     public void monad_law_right_side_identity_for_success() {
         Result<String, Integer> result = Result.success("OK");
-        assertThat(result.flatMap(Result::success)).isEqualTo(result);
+        assertThat(result.flatMap(r -> Result.success(r))).isEqualTo(result);
     }
 
     @Test
     public void monad_law_right_side_identity_for_error() {
         Result<String, Integer> result = Result.error(1);
-        assertThat(result.flatMap(Result::success)).isEqualTo(result);
+        assertThat(result.flatMap(r -> Result.success(r))).isEqualTo(result);
     }
 }
